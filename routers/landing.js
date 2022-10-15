7cdaf8491895ff420170beb42e35cd692b5347f0
Aayush Garg <aayushgarg2006@gmail.com>
2022-10-15 06:59:44 +0530

    update 6:59

--- routers/landing.js
+++ routers/landing.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../schemas/userSchema');
 
 router.get('/', (req, res)=>{
     if (req.isAuthenticated()) {
@@ -6,6 +7,15 @@ router.get('/', (req, res)=>{
     } else {
     res.render('landing', {user: req.user});
     }
+})
+
+router.get('/premium', (req, res)=>{
+    res.render('premium', {user: req.user});
+})
+
+router.get('/leaderboard', async (req, res)=>{
+    var users = await User.find({})
+    res.render('leaderboard', {user: req.user, users});
 })
 
 module.exports = router;
